d8b9563752469eca96e8a4de21685d29b7047b10
Alex Younger <alex@izoox.com>
2020-05-14 23:50:05 -0600

    IEX api working

--- fractalmarketslab/rescaledrange/api_import.py
+++ fractalmarketslab/rescaledrange/api_import.py
@@ -1,10 +1,7 @@
-import os
+from ..key import IEX_TOKEN
 import json
 from iexfinance.stocks import Stock
 
-# print(json.dumps(os.environ, indent=1))
-print(os.environ)
-print(os.environ.get('IEX_TOKEN'))
-print(os.getenv('IEX_TOKEN'))
-# tsla = Stock('TSLA', token=os.environ.get('IEX_TOKEN'))
-# tsla.get_price()
+
+tsla = Stock('TSLA', token=IEX_TOKEN)
+print(tsla.get_price())
