06973750be99facb9fd5e08dadcda9c133212ddc
Alex Younger <alex@izoox.com>
2020-11-24 00:19:36 -0700

    Updating trend chaser

--- lab/trend/chase_volume.py
+++ lab/trend/chase_volume.py
@@ -48,7 +48,7 @@ for i, chunk in enumerate(chunked_tickers):
             day5ChangePercent = stats['day5ChangePercent'] * 100 if ('day5ChangePercent' in stats and stats['day5ChangePercent']) else None
             # Critical
             week52high = stats['week52high'] if ('week52high' in stats and stats['week52high']) else 0
-            changeToday = quote['changePercent'] * 100 if ('changePercent' in quote and quote['changePercent']) else 0            
+            changeToday = quote['changePercent'] * 100 if ('changePercent' in quote and quote['changePercent']) else 0
             volume = quote['volume'] if ('volume' in quote and quote['volume']) else 0
             previousVolume = quote['previousVolume'] if ('previousVolume' in quote and quote['previousVolume']) else 0
 
@@ -77,9 +77,9 @@ for i, chunk in enumerate(chunked_tickers):
                     'ttmEPS': ttmEPS
                 },
             }
-            
+
             dynamicUpdateCreate(data_for_db, stock)
-            if ((fromHigh < 100) and (fromHigh > 70)):
+            if ((fromHigh < 100) and (fromHigh > 80)):
                 if (changeToday > 5):
                     if ((volume / previousVolume) > 3):
                         priceTargets = getPriceTarget(ticker)
@@ -87,23 +87,22 @@ for i, chunk in enumerate(chunked_tickers):
                         avgPricetarget = priceTargets['priceTargetAverage'] if (priceTargets and 'priceTargetAverage' in priceTargets) else None
                         highPriceTarget = priceTargets['priceTargetHigh'] if (priceTargets and 'priceTargetHigh' in priceTargets) else None
 
-
                         # Save Trends to DB
-                        Trend.objects.filter(stock=stock).update(                            
+                        Trend.objects.filter(stock=stock).update(
                             avgPricetarget=avgPricetarget,
                             highPriceTarget=highPriceTarget,
                             fromPriceTarget=fromPriceTarget,
                         )
-                    
+
                         keyStats = {
                             'week52': stats['week52high'],
                             'ttmEPS': ttmEPS,
                             'peRatio': stats['peRatio'],
-                            'day5ChangePercent': stats['day5ChangePercent'],
-                            'month1ChangePercent': stats['month1ChangePercent'],
-                            'ytdChangePercent': stats['ytdChangePercent'],
+                            'day5ChangePercent': stats['day5ChangePercent'] * 100 if ('day5ChangePercent' in stats) else None,
+                            'month1ChangePercent': stats['month1ChangePercent'] * 100 if ('month1ChangePercent' in stats) else None,
+                            'ytdChangePercent': stats['ytdChangePercent'] * 100 if ('ytdChangePercent' in stats) else None,
                             'day50MovingAvg': stats['day50MovingAvg'],
-                            'day200MovingAvg': stats['day200MovingAvg'],                            
+                            'day200MovingAvg': stats['day200MovingAvg'],
                             'highPriceTarget': highPriceTarget,
                             'fromPriceTarget': fromPriceTarget,
                             'fromHigh': fromHigh,
@@ -124,7 +123,7 @@ for i, chunk in enumerate(chunked_tickers):
 
                         stockData['volume'] = "{}K".format(volume / 1000)
                         stockData['previousVolume'] = "{}K".format(previousVolume / 1000)
-                        stockData['changeToday'] = changeToday    
+                        stockData['changeToday'] = changeToday
                         print('{} saved to Watchlist'.format(ticker))
                         results.append(stockData)
                         printTable(stockData)
