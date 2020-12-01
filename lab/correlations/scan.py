4f1357cca24a79408cd86b0be92a1c25e6bebc02
Alex Younger <alex@izoox.com>
2020-12-01 14:28:27 -0700

    Correlations working

--- lab/correlations/scan.py
+++ lab/correlations/scan.py
@@ -11,14 +11,34 @@ from dotenv import load_dotenv
 from ..twitter.tweet import send_tweet
 load_dotenv()
 django.setup()
-OUTPUT = "lab/correlations/output/"
-OUTPUTJSON = "lab/correlations/output_json/"
+FILES = "lab/correlations/output/files"
+JSON = "lab/correlations/output/json/correlations.json"
+
+HistoricalPrices = apps.get_model('database', 'HistoricalPrices')
+priceobjs = HistoricalPrices.objects.all()
+badset = ['GCOW', 'FAAR']
+json_output = {}
 
 
 def scan_output_folder():
-    files = os.listdir(OUTPUT)
+    files = os.listdir(FILES)
     return [f.split('.').pop(0) for f in files]
 
+def redundant_correlation(t1, t2, output):
+    if (output == 'json'):
+        if (t2 in json_output and json_output[t2]):
+            for data in json_output[t2]:
+                if (data['comparand'] == t1):
+                    return True
+        return False
+    if (output == 'files'):
+        if (t1 in scan_output_folder()):
+            with open(os.path.join(FILES, "{}.txt".format(t1))) as txtfile:
+                correlations = json.loads(txtfile.read())
+                for corrs in correlations:
+                    if (corrs['comparand'] == t2):
+                        return True
+        return False
 
 def correct_lengths(h1, h2):
     objs = [h1.prices, h2.prices]
@@ -41,7 +61,9 @@ def run_correlation(h1, output='files'):
         if h1 == h2:
             continue
         if (h2.stock.ticker in badset):
-            continue        
+            continue
+        if (redundant_correlation(h1.stock.ticker, h2.stock.ticker, output)):
+            continue
 
         if ((len(h1.prices) == len(h2.prices)) == False):
             x, y = correct_lengths(h1, h2)
@@ -61,23 +83,21 @@ def run_correlation(h1, output='files'):
         if (output == 'files'):
             correlations.append(result)
     if (output == 'files'):
-        with open(os.path.join(OUTPUT, '{}.txt'.format(h1.stock.ticker)), 'w') as txtfile:
+        with open(os.path.join(FILES, '{}.txt'.format(h1.stock.ticker)), 'w') as txtfile:
             json.dump(correlations, txtfile)
-    
-    
 
 
-HistoricalPrices = apps.get_model('database', 'HistoricalPrices')
-priceobjs = HistoricalPrices.objects.all()
-badset = ['GCOW', 'FAAR']
-json_output = {}
-print('Running...')
-for h in priceobjs:
-    if (h.stock.ticker in badset):
-        continue
-    print("Mass Correlating: {}".format(h.stock.ticker))
-    run_correlation(h)
+def scan_for_correlations(output='files'):
+    print('Running...')
+    for h in priceobjs:
+        if (output == 'files'):
+            if (h.stock.ticker in scan_output_folder()):
+                continue
+        if (h.stock.ticker in badset):
+            continue
+        print("Mass Correlating: {}".format(h.stock.ticker))
+        run_correlation(h, output)
 
-with open(os.path.join(OUTPUTJSON, 'correlations.json'), 'w') as json_file:
-    json.dump(json_output, json_file)
-
+    if (output == 'json'):
+        with open(JSON, 'w') as json_file:
+            json.dump(json_output, json_file)
