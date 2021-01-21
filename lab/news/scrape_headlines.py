7f68032d74999023b2d4b9d6222d7c4e101efadb
Alex Younger <alex@izoox.com>
2021-01-21 11:06:10 -0700

    Updating news scraper

--- lab/news/scrape_headlines.py
+++ lab/news/scrape_headlines.py
@@ -1,10 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
+from ..core.api import quoteStatsBatchRequest
+from ..core.functions import dataSanityCheck
+from ..core.output import printTable, printFullTable, writeCSV
+from ..fintwit.tweet import send_tweet
 import time
 import sys
 import json
 import os
+
 
 def blacklist(link):
     blacklist = ['www.nasdaq.com']
@@ -15,9 +20,75 @@ def blacklist(link):
 
     return skip
 
+
+def print_results(tickers):
+    print("\n")
+    results = []
+    batch = quoteStatsBatchRequest(tickers)
+
+    for ticker, stockinfo in batch.items():
+
+        if (stockinfo.get('quote', False) and stockinfo.get('stats', False)):
+            quote = stockinfo.get('quote')
+            stats = stockinfo.get('stats')
+            price = quote.get('latestPrice', 0)
+
+            ttmEPS = stats.get('ttmEPS', None)
+            day5ChangePercent = round(dataSanityCheck(stats, 'day5ChangePercent') * 100, 2)
+            month1ChangePercent = round(dataSanityCheck(stats, 'month1ChangePercent') * 100, 2)
+            ytdChangePercent = round(dataSanityCheck(stats, 'ytdChangePercent') * 100, 2)
+            volume = dataSanityCheck(quote, 'volume')
+            previousVolume = dataSanityCheck(quote, 'previousVolume')
+            changeToday = round(dataSanityCheck(quote, 'changePercent') * 100, 2)
+
+            # Critical
+            week52high = dataSanityCheck(stats, 'week52high')
+
+            critical = [week52high, volume, previousVolume]
+
+            if ((0 in critical)):
+                continue
+
+            fromHigh = round((price / week52high) * 100, 3)
+            volumeChangeDay = (float(volume) - float(previousVolume)) / float(previousVolume) * 100
+
+            keyStats = {
+                'ticker': ticker,
+                'name': stats['companyName'],
+                'lastPrice': price,
+                'peRatio': stats.get('peRatio', None),
+                'week52': week52high,
+                'changeToday': changeToday,
+                'day5ChangePercent': day5ChangePercent if day5ChangePercent else None,
+                'month1ChangePercent': month1ChangePercent if month1ChangePercent else None,
+                'ytdChangePercent': ytdChangePercent if ytdChangePercent else None,
+                'volumeChangeDay':  "{}%".format(round(volumeChangeDay, 2)),                
+                'fromHigh': fromHigh,
+                'ttmEPS': ttmEPS
+            }
+
+            results.append(keyStats)
+
+    if results:
+        today = date.today().strftime('%m-%d')
+        writeCSV(results, 'lab/news/output/appeared_in_news_{}.csv'.format(today))
+
+        printFullTable(results, struct='dictlist')
+
+        # Tweet
+        # tweet = ""
+        # for i, data in enumerate(results):
+        #     ticker = '${}'.format(data['ticker'])
+        #     changeToday = data['changeToday']
+        #     tweet_data = "{} +{}% \n".format(ticker, changeToday)
+        #     tweet = tweet + tweet_data
+
+        # send_tweet(tweet, True)
+
+
 def scrape_news(query="best+stocks+to+buy+this+week"):
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}    
-    stocklinks = []
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    tickers = []
 
     try:
         url = 'https://www.bing.com/news/search?q={}'.format(query)
@@ -32,21 +103,18 @@ def scrape_news(query="best+stocks+to+buy+this+week"):
         if (blacklist(link['href'])):
             continue
 
-        print(link['href'])
+        print("Searching... "+link['href'])
 
         page = requests.get(link['href'], headers=headers)
         soup = BeautifulSoup(page.text, 'html.parser')
         all_links = soup.find_all("a")
 
-        # print(len(all_links))
         for l in all_links:
             if ('NASDAQ:' in l.text):
-                stocklinks.append(l)
+                tickers.append(l.text.split(':')[1])
             if ('NYSE:' in l.text):
-                stocklinks.append(l)
+                tickers.append(l.text.split(':')[1])
 
-        time.sleep(0.5)
+        time.sleep(1)
 
-    print("\n")
-    for stock in stocklinks:        
-        print(stock.text)
+    print_results(tickers)
