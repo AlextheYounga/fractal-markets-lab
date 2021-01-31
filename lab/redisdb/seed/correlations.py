b162d4348f92f336403dfc21fc5c3a0dcc30ca73
Alex Younger <alex@hazlitt.tech>
2021-01-31 02:06:18 -0700

    Testing multiprocessing

---
+++ lab/redisdb/seed/correlations.py
@@ -0,0 +1,38 @@
+import django
+from django.apps import apps
+import json
+import redis
+import sys
+import os
+from dotenv import load_dotenv
+load_dotenv()
+django.setup()
+
+r = redis.Redis(host='localhost', port=6379, db=0, charset="utf-8", decode_responses=True)
+
+Stock = apps.get_model('database', 'Stock')
+Correlation = apps.get_model('database', 'Correlation')
+HistoricalPrices = apps.get_model('database', 'HistoricalPrices')
+correlations = Correlation.objects.all()
+stocks = Stock.objects.all()
+
+def calculate_datapoints(corr):
+    h1 = HistoricalPrices.objects.get(stock=corr.stock)
+    s2 = Stock.objects.get(ticker=corr.comparand)
+    h2 = HistoricalPrices.objects.get(stock=s2)
+
+    counts = [len(h1.prices), len(h2.prices)]
+    dp = min(counts)
+
+    return dp
+
+def save_data(corr):    
+    t1 = corr.stock.ticker
+    t2 = corr.comparand
+    rv = corr.rvalue
+    dp = calculate_datapoints(corr)
+    print(t1+'-'+t2)
+
+    r.set('correlation-'+t1+'-'+t2+'-rvalue', (corr.rvalue if corr.rvalue else ""))
+    r.set('correlation-'+t1+'-'+t2+'-datapoints', dp)
+    
--- lab/redisdb/tests.py
+++ lab/redisdb/tests.py
@@ -1,3 +1,9 @@
 import redis
 
 r = redis.Redis(host='localhost', port=6379, db=0)
+r.set('foo', 'bar')
+for key in r.scan_iter("user:*"):
+    v = r.get(key)
+    print(v)
+    # get = r.get('foo')
+    # print(get)
