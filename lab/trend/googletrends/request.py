41a91f221ee836c60aa249eed2e6ce94e2c6eeb9
Alex Younger <alex@izoox.com>
2021-02-11 16:59:04 -0700

    Setting up gtrends

--- lab/trend/googletrends/request.py
+++ lab/trend/googletrends/request.py
@@ -1,8 +1,10 @@
 from pytrends.request import TrendReq
 
-pytrends = TrendReq(hl='en-US', tz=360)
-kw_list = ["gsat stock"]
-pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
-interest = pytrends.interest_over_time()
 
-print(interest)
+def stock_search_trends(ticker):
+    pytrends = TrendReq(hl='en-US', tz=360)
+    kw_list = [ticker+' stock']
+    pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+    interest = pytrends.interest_over_time()
+
+    print(interest)
