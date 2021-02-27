cdd4b499bff1dd259cae77d8b46fc3605ee0f6ec
Alex Younger <alex@hazlitt.tech>
2021-02-27 01:13:41 -0700

    Custom parse arg working

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-import sys, getopt
+import sys
 import colored
 from colored import stylize
 from lab.core.output import printTabs, printFullTable
@@ -9,116 +9,156 @@ load_dotenv()
 
 def list_commands():
     headers = ['Command', 'Description']
-    print("\n")
+    print('\n')
     print('Available Subcommands')
     print('No quotes required on [<ticker>] arguments, may be typed directly into the terminal.')
-    print("\n\n")
+    print('\n\n')
 
     commands = [
-        ['donchian [<ticker>] [--days=30] [--tweet]', 'Runs a donchian range calculation on a ticker'],
+        ['donchian [<ticker>] [days=30] [tweet]', 'Runs a donchian range calculation on a ticker'],
         ['financials [<ticker>]', 'Returns financials data for ticker, including some custom indicators not provided by IEX.'],
-        ['macro:trends [--timeframe=1m] [--gain=20]', 'Scans all ETFs and returns the ETFs with the performance above an int (gain) within a timerange (5d, 1m, 3m, 1y)'],
+        ['macro:trends [timeframe=1m] [gain=20]', 'Scans all ETFs and returns the ETFs with the performance above an int (gain) within a timerange (5d, 1m, 3m, 1y)'],
         ['macro:gainers', 'Scans all ETFs and returns ETFs with highest day change.'],
-        ['news:scrape [--query=insert+string]', 'Searches a query and searches first 10 articles for stocks mentioned in article'],
-        ['hurst [<ticker>] [--output=table]', 'Runs a rescaled range analysis on a ticker. Output defaults to table.'],
-        ['range [<ticker>] [--tweet]', 'Runs a volatility range analysis on a ticker.'],
+        ['news:scrape [query=insert+string]', 'Searches a query and searches first 10 articles for stocks mentioned in article'],
+        ['hurst [<ticker>] [output=table]', 'Runs a rescaled range analysis on a ticker. Output defaults to table.'],
+        ['range [<ticker>] [tweet]', 'Runs a volatility range analysis on a ticker.'],
         ['historicalprices:get [<ticker>]', 'Fetches historical prices for a ticker and saves them to db.'],
-        ['inflation:calculate [--update]', 'Inflation index using etfs'],
-        ['inflation:graph [--update]', 'Graph inflation index using etfs'],
-        ['inflation:functions [--refresh]', 'Grabs max historical prices for all etfs in sectors list, updates with fresh data.'],
-        ['trend:chase [--pennies]', 'Scans all stocks and returns todays gainers with above certain thresholds (weeds out the penny stocks).'],
-        ['trend:search [--string=]', 'Scans stocks with string in stock name and looks for gainers'],
+        ['inflation:calculate [update]', 'Inflation index using etfs'],
+        ['inflation:graph [update]', 'Graph inflation index using etfs'],
+        ['inflation:functions [refresh]', 'Grabs max historical prices for all etfs in sectors list, updates with fresh data.'],
+        ['trend:chase [pennies]', 'Scans all stocks and returns todays gainers with above certain thresholds (weeds out the penny stocks).'],
+        ['trend:search [string=]', 'Scans stocks with string in stock name and looks for gainers'],
         ['trend:earnings', 'Scans all stocks and returns todays gainers who have consistently good earnings.'],
         ['trend:streak [<ticker>]', 'Determines the current winning/losing streak for a ticker'],
         ['trend:gainers', 'Grabs todays gainers and checks their earnings.'],
         ['trend:google', 'Searches google trends for search query interest'],
-        ['pricedingold [<ticker>][--timespan=5y][--test=False]', 'Graphs and assets price in gold.'],
-        ['vol:graph [<ticker>] [--ndays=30]', 'Graphs vol'],
+        ['pricedingold [<ticker>][timespan=5y][test=False]', 'Graphs and assets price in gold.'],
+        ['vol:graph [<ticker>] [ndays=30]', 'Graphs vol'],
         ['volume:chase', 'Scans all stocks and returns todays gainers with abnormally high volume.'],
         ['volume:anomaly', 'Scans all stocks and returns stocks who are accumulating extremely high volume over the last week. Finds market singularities.'],
-        ['volume:graph [<ticker>][--timeframe=3m][--sandbox=false]', 'Scans all stocks and returns stocks who are accumulating extremely high volume over the last week. Finds market singularities.'],
+        ['volume:graph [<ticker>][timeframe=3m][sandbox=false]', 'Scans all stocks and returns stocks who are accumulating extremely high volume over the last week. Finds market singularities.'],
         ['vix [<ticker>]', 'Runs the VIX volatility equation on a ticker'],
         ['output:last', 'Returns the last cached output, can resort by specific key.'],
         ['rdb:export', 'Exports redisdb to zipped json file'],
         ['rdb:import', 'Import redisdb from a zipped json file'],
-
     ]
     printTabs(commands, headers, 'simple')
-    print("\n\n")
+    print('\n\n')
 
 
 def command_error(required={}, opt=None):
     if(not (required or opt)):
-        print(stylize("Error: your command did not match any known programs. Closing...", colored.fg("red")))
-        print("\n")
+        print(stylize('Error: your command did not match any known programs. Closing...', colored.fg('red')))
+        print('\n')
         return
 
     if (required):
-        print(stylize("FAILED: Requires arguments: ", colored.fg("red")))
-        for typ, var in required.items():
-            print(stylize("({}) [{}]".format(typ, var), colored.fg("red")))
-        print("\n")
+        print(stylize('FAILED: Requires arguments: ', colored.fg('red')))
+        for var, rules in required.items():
+            print(stylize('({}) [{}] in position {}'.format(rules['type'], var, rules['pos']), colored.fg('red')))
+        print('\n')
     if (opt):
-        print(stylize("Optional arguments: ", colored.fg("yellow")))
+        print(stylize('Optional arguments: ', colored.fg('yellow')))
         if (isinstance(opt, dict)):
-            for typ, var in opt.items():
-                print(stylize("({}) [{}]".format(typ, var), colored.fg("yellow")))
+            for var, typ in opt.items():
+                print(stylize('({}) [{}]'.format(var, typ), colored.fg('yellow')))
         if (isinstance(opt, list)):
             for var in opt.items():
-                print(stylize("[{}]".format(var), colored.fg("yellow")))
-            print("\n")
+                print(stylize('[{}]'.format(var), colored.fg('yellow')))
+            print('\n')
+
+
+def parse_args(args, required, opt):
+    params = {}
+
+    for req, rules in required.items():
+        if ('=' in args[rules['pos']]):
+            rv = args[rules['pos']].split('=')[1]
+        else:
+            rv = args[rules['pos']]
+
+        params[req] = rv
+
+    if (required and params == {}):
+        command_error()
+    if (opt):
+        for var, rules in opt.items():
+            in_args = [var == arg.split('=')[0] for arg in args]
+
+            if (True in in_args):                
+                if (rules['type'] == bool):
+                    if ('--' in var):
+                        var = var.split('--')[1]
+                    if ('=' in var):
+                        argvalue = var.split('=')[1]
+                    else:
+                        argvalue = True
+
+                    params[var] = argvalue
+                    continue
+
+                argvalue = args[in_args.index(True)].split('=')[1]
+
+                if (rules['type'] == int and isinstance(int(argvalue), int)):
+                    params[var] = int(argvalue)
+                    continue
+                else:
+                    print(stylize(var+' must be of type int.', colored.fg('red')))
+                    sys.exit()
+
+                params[var] = argvalue
+
+    return params
 
 
 def donchian_controller(args):
-    required = {"string": "ticker"}
-    opt = ["--days", "--tweet"]
+    required = {'ticker': {'pos': 0, 'type': str}}
+    opt = {
+        'days': {'type': int, 'default': 30},
+        '--tweet': {'type': bool, 'default': False}
+    }
 
     if (not args):
         command_error(required, opt)
         return
 
+    params = parse_args(args, required, opt)
+
     from lab.donchian.range import calculate
-    ticker = args[0]
-    try:        
-        tweet = True if (args[1] == '--tweet') else False
-        print(calculate(ticker, tweet))
-        return
-    except IndexError:
-        print(calculate(ticker))
-        return
-    command_error()
+    print(calculate(
+        params['ticker'],
+        days=params['days'] if 'days' in params else opt['days']['default'],
+        tweet=True if ('tweet' in params) else opt['--tweet']['default']
+    ))
 
 
 def inflation_controller(subroutine, args=[]):
     if (subroutine == 'graph'):
+        opt = {'--update': {'type': bool, 'default': False}}
+        params = parse_args(args, opt=opt)
+
         from lab.inflation.measure import graph
-
-        try:
-            update = True if (args and args[0] == '--update') else False
-            print(graph(update))
-            return
-
-        except IndexError:
-            print(graph())
-            return
+        print(graph(
+            update=params['update'] if ('update' in params) else opt['--update']['default']
+        ))
+        return
 
     if (subroutine == 'calculate'):
+        opt = {'--update': {'type': bool, 'default': False}}
+        params = parse_args(args, opt=opt)
+
         from lab.inflation.measure import annual
 
-        try:
-            update = True if (args[0] == '--update') else False
-            print(annual(update))
-            return
-
-        except IndexError:
-            print(annual())
-            return
+        print(annual(
+            update=params['update'] if ('update' in params) else opt['--update']['default']
+        ))
+        return
 
     command_error()
 
 
 def financials_controller(args):
-    required = {"string": "ticker"}
+    required = {'string': 'ticker'}
 
     if (not args):
         command_error(required)
@@ -131,84 +171,78 @@ def financials_controller(args):
 
 def macro_controller(subroutine, args=[]):
     if (subroutine == 'trends'):
+        opt = {
+            'timeframe': {'type': str, 'default': '1m'},
+            'gain': {'type': int, 'default': 20}
+        }
+        params = parse_args(args, opt=opt)
         from lab.macro.trends import calculate_trends
 
-        try:
-            timeframe = args[0].split('--')[1]
-            gain = args[1].split('--')[1]
-            print(calculate_trends(timeframe, gain))
-            return
-
-        except IndexError:
-            try:
-                timeframe = args[0].split('--')[1]
-                print(calculate_trends(timeframe))
-                return
-            except IndexError:
-                print(calculate_trends)
-                return
+        print(calculate_trends(
+            timeframe=params['timeframe'] if ('timeframe' in params) else opt['timeframe']['default'],
+            gain=params['gain'] if ('gain' in params) else opt['gain']['default'],
+        ))
+        return
 
     if (subroutine == 'gainers'):
         import lab.macro.gainers
+        return
 
     command_error()
 
 
 def news_controller(subroutine, args=[]):
     if (subroutine == 'scrape'):
+        opt = {'query': {'type': str, 'default': 'best+stocks+to+buy'}, }
+
         from lab.news.scrape_headlines import scrape_news
 
-        try:
-            query = args[0].split('--')[1]
-            print(scrape_news(query))
-            return
-        except IndexError:
-            print(scrape_news())
-            return
+        params = parse_args(args, opt=opt)
+
+        print(scrape_news(
+            query=params['query'] if ('query' in params) else opt['query']['default'],
+        ))
+        return
+
     command_error()
 
 
 def pricedingold_controller(args):
     from lab.pricedingold.compare import price_in_gold
-    required = {"string": "ticker"}
-    opt = {"string": "--timeframe"}
+    required = {'ticker': {'pos': 0, 'type': str}}
+    opt = {
+        'timeframe': {'type': str, 'default': '5y'},
+        'sandbox': {'type': bool, 'default': False}
+    }
 
     if (not args):
         command_error(required, opt)
         return
-    try:
-        ticker = args[0]
-        timeframe = args[1].split('--')[1]
-        print(price_in_gold(ticker, timeframe))
-        return
-    except IndexError:
-        try:
-            ticker = args[0]
-            print(price_in_gold(ticker))
-            return
-        except IndexError:
-            print(price_in_gold)
-            return
+
+    params = parse_args(args, required, opt)
+
+    print(price_in_gold(
+        ticker=params['ticker'],
+        timeframe=params['timeframe'] if ('timeframe' in params) else opt['timeframe']['default'],
+        sandbox=params['sandbox'] if ('sandbox' in params) else opt['sandbox']['default'],
+    ))
 
 
 def hurst_controller(args):
-    required = {"string": "ticker"}
-    opt = {"string": "--output"}
+    required = {'ticker': {'pos': 0, 'type': str}}
+    opt = {'output': {'type': str, 'default': 'table'}}
 
     if (not args):
         command_error(required, opt)
         return
 
     from lab.rescaledrange.fractal_calculator import fractal_calculator
-    ticker = args[0]
+    params = parse_args(args, required, opt)
 
-    try:
-        output = args[1].split('--')[1]
-        print(fractal_calculator(ticker, output))
-        return
-    except IndexError:
-        print(fractal_calculator(ticker))
-        return
+    print(fractal_calculator(
+        ticker=params['ticker'],
+        output=params['output'] if ('output' in params) else opt['output']['default'],
+    ))
 
 
 def rdb_controller(subroutine, args=[]):
@@ -221,44 +255,39 @@ def rdb_controller(subroutine, args=[]):
 
 
 def range_controller(args):
-    from lab.riskrange.lookup import rangeLookup
-    required = {"string": "ticker"}
-    opt = ["--tweet"]
+
+    required = {'ticker': {'pos': 0, 'type': str}}
+    opt = {'--tweet': {'type': bool, 'default': False}}
 
     if (not args):
         command_error(required, opt)
         return
 
-    ticker = args[0]
-    try:
-        tweet = True if (args[1] == '--tweet') else False
-        print(rangeLookup(ticker, tweet))
-        return
-    except IndexError:
-        print(rangeLookup(ticker))
-        return
+    from lab.riskrange.lookup import rangeLookup
+
+    params = parse_args(args, required, opt)
+
+    print(rangeLookup(
+        ticker=params['ticker'],
+        tweet=params['tweet'] if ('tweet' in params) else opt['--tweet']['default'],
+    ))
 
 
 def output_controller(subroutine, args):
     if (subroutine == 'last'):
         from lab.redisdb.controller import fetch_last_output
 
-        try:
-            filterKey = args[0].split('--')[1]
-            results = fetch_last_output(filterKey)
-            printFullTable(results, struct='dictlist')
-            return
-        except IndexError:
-            results = fetch_last_output()
-            printFullTable(results, struct='dictlist')
-            return
+        results = fetch_last_output()
+        printFullTable(results, struct='dictlist')
+        return
+
     command_error()
 
 
 def trend_controller(subroutine, args):
 
     if (subroutine == 'streak'):
-        required = {"string": "ticker"}
+        required = {'ticker': {'pos': 0, 'type': str}}
         if (not args):
             command_error(required)
             return
@@ -268,21 +297,26 @@ def trend_controller(subroutine, args):
         return
 
     if (subroutine == 'search'):
-        required = {"string": "query"}
+        required = {'query': {'pos': 0, 'type': str}}
+
         if (not args):
             command_error(required)
             return
+
         from lab.trend.chase.search import search
-        searchstring = args[0].split('=')[1]
-        print(search(searchstring))
+        params = parse_args(args, required)
+
+        print(search(params['query']))
         return
 
     if (subroutine == 'chase'):
         from lab.trend.chaser import chase_trends
-        if (args):
-            print(chase_trends(args[0]))
-            return
-        print(chase_trends())
+        opt = {'pennies': {'type': bool, 'default': False}}
+        params = parse_args(args, opt=opt)
+        print(chase_trends(
+            pennies=params['pennies'] if ('pennies' in params) else opt['pennies']['default']
+        ))
+        return
 
     if (subroutine == 'earnings'):
         import lab.trend.chase.earnings
@@ -300,28 +334,32 @@ def trend_controller(subroutine, args):
 
 
 def vol_controller(subroutine, args):
-    required = {"string": "ticker"}
-    opt = {"string": "--ndays="}
+    required = {'ticker': {'pos': 0, 'type': str}}
+    opt = {'ndays': {'type': int, 'default': 30}}
 
     if (not args):
         command_error(required, opt)
         return
+
     if (subroutine == 'graph'):
         from lab.vol.calculator import graphVol
-        ticker = args[0]
-        try:
-            print(graphVol(ticker, ndays=args[1]))
-        except IndexError:
-            print(graphVol(ticker))
-        return
+
+        params = parse_args(args, required, opt=opt)
+
+        print(graphVol(
+            params['ticker'],
+            ndays=params['ndays'] if ('ndays' in params) else opt['ndays']['default']
+        ))
 
 
 def volume_controller(subroutine, args):
     if (subroutine == 'graph'):
-        required = {"string": "ticker"}
+        required = {'ticker': {'pos': 0, 'type': str}}
+
         if (not args):
             command_error(required)
             return
+
         from lab.volume.graph import graph_volume
         print(graph_volume(args[0]))
 
@@ -333,8 +371,8 @@ def volume_controller(subroutine, args):
 
 
 def vix_controller(args):
-    required = {"string": "ticker"}
-    opt = {"string": "--debug"}
+    required = {'ticker': {'pos': 0, 'type': str}}
+    opt = {'--debug': {'type': bool, 'default': False}}
 
     if (not args):
         command_error(required, opt)
@@ -342,14 +380,12 @@ def vix_controller(args):
 
     from lab.vix.equation import vix_equation
 
-    ticker = args[0]
+    params = parse_args(args, required, opt)
 
-    if (len(args) >= 2):
-        debug = True if (args[1] == '--debug') else False
-        print(vix_equation(ticker, debug))
-        return
-
-    print(vix_equation(ticker))
+    print(vix_equation(
+        params['ticker'],
+        debug=params['debug'] if ('debug' in params) else opt['--debug']['default']
+    ))
 
 
 def main():
@@ -364,13 +400,13 @@ def main():
 
     if (':' in args[0]):
         command = args.pop(0)
-        program = command.split(':')[0] + "_controller"
+        program = command.split(':')[0] + '_controller'
         subroutine = command.split(':')[1]
 
         globals()[program](subroutine, args)
         return
     else:
-        program = args.pop(0) + "_controller"
+        program = args.pop(0) + '_controller'
 
         globals()[program](args)
         return
