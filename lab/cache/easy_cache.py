ec306f10f4be4edeed76e7b64e109a9b0e486a69
alextheyounger <alex@hazlitt.tech>
2021-07-05 01:06:53 -0400

    Cache is working

--- lab/cache/easy_cache.py
+++ lab/cache/easy_cache.py
@@ -1,7 +1,6 @@
 import json
 from colored import stylize
 from ..core.functions import zipfolder
-from .functions import *
 import cryptography
 import sys
 import os
@@ -12,12 +11,13 @@ class EasyCache:
     # My own storage system because this is just how I am.
     def __init__(self):
         self.path = 'lab/cache/ez'  # better for complex data
-        self.easy_path = 'lab/cache/ez/ez.json'  # for simple data
+        self.json_path = f"{self.path}/ez.json"  # for simple data
+        self.ledger_path = f"{self.path}/ledger.json"  # for simple data
 
     def get(key):
         path = pathfinder(EasyCache().path, key)
         if os.path.exists(path):
-            return readTxtFile(path)
+            return readTxtFile(path, key)
 
         with open(path) as jsonfile:
             cache = json.loads(jsonfile.read())
@@ -25,35 +25,125 @@ class EasyCache:
         jsonfile.close()
         return cache.get(key, False)
 
-    def put(key, value, method='easy'):
+    def put(key, value):
+        path = pathfinder(EasyCache().path, key)
+        writeTxtFile(key, value, path)
+        return True
 
-        if (method == 'easy'):
-            path = EasyCache().path
-            with open(path) as jsonfile:
-                cache = json.loads(jsonfile.read())
-            jsonfile.close()
+    def quickstore(key, value):
+        path = EasyCache().path
+        with open(path) as jsonfile:
+            cache = json.loads(jsonfile.read())
+        jsonfile.close()
 
-            cache[key] = value
+        cache[key] = value
 
-            with open(path, 'w') as jsonfile:
-                json.dump(cache, jsonfile)
-            jsonfile.close()
+        with open(path, 'w') as jsonfile:
+            json.dump(cache, jsonfile)
+        jsonfile.close()
 
-            return True
-
-        if (method == 'complex'):
-            path = pathfinder(EasyCache().path, key)
-            # print(path)
-            # sys.exit()
-            writeTxtFile(value, path)
-            return True
+        return True
 
     def clear():
         ez = EasyCache()
         os.remove(ez.path)
         os.mkdir(ez.path)
-        with open(ez.easy_path, 'w') as jsonfile:
+        with open(ez.json_path, 'w') as jsonfile:
             json.dump({}, jsonfile)
-        jsonfile.close()
+        with open(ez.ledger_path, 'w') as jsonfile:
+            json.dump({}, jsonfile)
 
         print(stylize('Cache cleared!', colored.fg('green')))
+
+
+def pathfinder(path, key):
+    separators = ['-', '.', '_', ':', '->']
+    for sep in separators:
+        if (sep in key):
+            levels = key.split(sep)
+
+            for level in levels:
+                path = path + f"/{level}"
+                if not os.path.exists(path):
+                    os.mkdir(path)
+            break
+
+    path = path + f"/{path.split('/')[-1]}.txt"
+
+    return path
+
+
+def readLedger():
+    ledger = open(EasyCache().ledger_path, "r")    
+    return json.loads(ledger.read())
+
+
+def writeLedger(key, fmt):
+    ledger = readLedger()
+    ledger[key] = str(fmt)
+    with open(EasyCache().ledger_path, "w") as jsonfile:
+        jsonfile.write(json.dumps(ledger))
+
+
+def checkFormat(key):
+    ledger = readLedger()
+    return ledger.get(key, False)
+
+
+def readTxtFile(path, key):
+    txtfile = open(path, "r")
+
+    fmt = checkFormat(key)
+    
+    if (fmt):
+        if (fmt == "<class 'list'>"):
+            items = []
+            txtfile = open(path, "r")
+            for line in txtfile:                
+                stripped_line = line.strip()
+                line_list = stripped_line.split()
+                items.append(str(line_list[0]))
+
+            return list(dict.fromkeys(items))
+
+        if (fmt == "<class 'str'>"):
+            txtfile = open(path, "r")            
+            return txtfile.read()
+
+        if (fmt == "<class 'dict'>"):
+            txtfile = open(path, "r")
+            return json.loads(txtfile.read())
+
+
+def writeTxtFile(key, data, path, append=False):
+    # Appending function
+    if (append):
+        checked = readTxtFile(path, key)
+        if (checked):
+            lst = list(dict.fromkeys(checked + data))
+
+    fmt = type(data)
+    writeLedger(key, fmt)
+    txtfile = path
+    # os.remove(txtfile)
+    with open(txtfile, 'w') as f:        
+        if (fmt == list):
+            for item in lst:
+                f.write("%s\n" % item)
+        if (fmt == str):
+            f.write(data)
+        if (fmt == dict):
+            f.write(json.dumps(data))
+
+    return True
+
+
+def deleteFromTxTFile(lst, path):
+    read = readTxtFile(path)
+    for l in lst:
+        read.remove(l)
+
+    os.remove(path)
+    with open(path, 'w') as f:
+        for item in read:
+            f.write("%s\n" % item)
--- lab/cache/test.py
+++ lab/cache/test.py
@@ -14,6 +14,26 @@ data = {
   "volatility": 29,
   "daysToExpiration": 0,
   "numberOfContracts": 4502,
+  "1symbol": "SPY",
+  "1status": "SUCCESS",
+  "1underlying": None,
+  "1strategy": "SINGLE",
+  "1interval": 0,
+  "1isDelayed": True,
+  "1isIndex": False,
+  "1interestRate": 0.1,
+  "1underlyingPrice": 388.245,
+  "1volatility": 29,
+  "1daysToExpiration": 0,
+  "1numberOfContracts": 4502,
 }
-    
-EasyCache.get('test.data.chain')
+# JSON = 'lab/vix/sample_response/response.json'
+# with open(JSON) as jsonfile:
+#     chain = json.loads(jsonfile.read())
+
+# data = 'This is a test'
+
+# print()
+
+# EasyCache.put('test.data.string', data)
+print(EasyCache.get('test.data.string'))
